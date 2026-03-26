838ddbc0dffb6f8e18f2a3143742545c8ea20045
Benjamin Luton <benjamin.luton@gmail.com>
2026-03-26 22:52:33 +1000

    Docker Examples

--- builder-scripts/preload-cache.sh
+++ builder-scripts/preload-cache.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "########################################"
-echo "[INFO] Downloading ComfyUI & Nodes..."
+echo "[INFO] Downloading ComfyUI"
 echo "########################################"
 
 cd /default-comfyui-bundle
@@ -12,11 +12,3 @@ cd /default-comfyui-bundle/ComfyUI
 # Using stable version (has a release tag)
 git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
 
-# Force ComfyUI-Manager to use PIP instead of UV
-mkdir -p /default-comfyui-bundle/ComfyUI/user/__manager
-
-cat <<EOF > /default-comfyui-bundle/ComfyUI/user/__manager/config.ini
-[default]
-use_uv = False
-security_level = weak
-EOF
